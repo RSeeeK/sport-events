3a3e1d139fe63db8a826eb0b973f1976c22c9e4a
github_main <AGRabadanov@gmail.com>
2021-05-30 15:00:16 +0300

    fix gradle wrapper

--- src/test/java/ru/devhack/motomoto/sportevents/SportEventsApplicationTests.java
+++ src/test/java/ru/devhack/motomoto/sportevents/SportEventsApplicationTests.java
@@ -2,8 +2,10 @@ package ru.devhack.motomoto.sportevents;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class SportEventsApplicationTests {
 
 	@Test
